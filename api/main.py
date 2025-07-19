b9b9868e0cf2e865445a99c453680039c2f3da6a
Felipe Durar <felipe.malaquias@venturus.org.br>
2025-07-19 13:35:10 -0300

    Replaced the deprecated on_event with the FastAPI's lifespan protocol, created a custom gitignore that ignores also the data folder

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 
 from api.config import settings
 from api.db import init_db, engine
@@ -10,29 +11,28 @@ from api.routers import books, auth
 from api.services.user_service import UserService
 from api.tasks import scrape_and_store
 
-app = FastAPI(
-    title="Book Scraper API",
-    version="0.1.0",
-    description="API for querying books via scraping",
-    openapi_url="/api/v1/openapi.json",
-    docs_url="/api/v1/docs"
-)
-
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["GET", "POST"],
-    allow_headers=["*"],
-)
-
-app.include_router(books.router, prefix="/api/v1/books", tags=["Books"])
-app.include_router(auth.router)
-
 scheduler = BackgroundScheduler(timezone="America/Sao_Paulo")
 
-@app.on_event("startup")
-def start_scheduler():
+def setup_database():
+    print("Seeting Up Database...")
+
+    # 1) create tables
+    init_db()
+
+    # 2) seed admin user if missing
+    from sqlmodel import Session
+
+    with Session(engine) as session:
+        user_service = UserService(session)
+        admin = user_service.get_user_by_username("admin")
+        if not admin:
+            # choose a secure default or read from env
+            default_password = settings.ADMIN_PASSWORD
+            user_service.create_user(username="admin", password=default_password, is_admin=True)
+            print("Admin user created with username='admin' and password=", default_password)
+
+def setup_scheduler():
+    print("Seeting Up Scheduler...")
     scheduler.add_job(
         scrape_and_store,
         trigger="date",
@@ -50,27 +50,35 @@ def start_scheduler():
     )
     scheduler.start()
 
-@app.on_event("shutdown")
-def shutdown_scheduler():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
+    setup_database()
+    setup_scheduler()
+    print("Setup Completed!")
+    yield
+
+    # Shutdown logic
     scheduler.shutdown()
 
-@app.on_event("startup")
-def on_startup():
-    # 1) create tables
-    init_db()
+app = FastAPI(
+    title="Book Scraper API",
+    version="0.1.0",
+    description="API for querying books via scraping",
+    openapi_url="/api/v1/openapi.json",
+    docs_url="/api/v1/docs",
+    lifespan=lifespan
+)
 
-    # 2) seed admin user if missing
-    from sqlmodel import Session
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
-    with Session(engine) as session:
-        user_service = UserService(session)
-        admin = user_service.get_user_by_username("admin")
-        if not admin:
-            # choose a secure default or read from env
-            default_password = settings.ADMIN_PASSWORD
-            user_service.create_user(username="admin", password=default_password, is_admin=True)
-            print("Admin user created with username='admin' and password=", default_password)
-
+app.include_router(books.router, prefix="/api/v1/books", tags=["Books"])
+app.include_router(auth.router)
 
 # --- Healthcheck ---
 @app.get("/api/v1/health", tags=["Health"], status_code=200)
